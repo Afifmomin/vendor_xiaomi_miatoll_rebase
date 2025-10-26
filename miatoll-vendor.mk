78d3d77e20df3fc5b65dafac76a75753254805d4
Afifmomin <afifmomin123@gmail.com>
2025-10-26 08:45:50 -0700

    Revert "miatoll: audio: utilize `PCM_24_BIT_PACKED` for speaker"
    This reverts commit 4c3b476914ce6d9b40390d453d57089e5b74705f.

--- miatoll-vendor.mk
+++ miatoll-vendor.mk
@@ -389,7 +389,6 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.wifidisplaysession@1.0_vendor \
     btaudio_offload_if \
     audio.bluetooth_qti.default \
-    audio.primary.atoll \
     vendor.qti.hardware.bluetooth_audio@2.0-impl \
     vendor.qti.hardware.bluetooth_audio@2.1-impl \
     libFileMux_proprietary \
@@ -399,7 +398,6 @@ PRODUCT_PACKAGES += \
     libadiertac \
     libadm \
     libaudcal \
-    libaudio_log_utils \
     libaudioconfigstore \
     libaudioparsers \
     libbluetooth_audio_session_qti \
@@ -420,7 +418,6 @@ PRODUCT_PACKAGES += \
     libmulawdec \
     libqcodec2 \
     libqtigef \
-    libssrec \
     libsurround_3mic_proc \
     libwfdcodecv4l2_proprietary \
     libwfdcommonutils_proprietary \
